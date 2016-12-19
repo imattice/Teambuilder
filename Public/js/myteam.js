484fe1a120ee5e487eac10ca48cdacf87af1c3e4
Ike Mattice <gogreen37@me.com>
2016-12-18 21:40:28 -0800

    add more styling.

--- Public/js/myteam.js
+++ Public/js/myteam.js
@@ -1,15 +1,33 @@
+
+
 $(document).ready(function() {
   // console.log("works");
+
+// function showDetail(this) {
+//   $(this).find(".gender").show();
+//   $(this).find(".ability").show();
+//   $(this).find(".nature").show();
+//   $(this).find(".stats").show();
+//   $(this).find(".moves").show();
+//   $(this).find(".edit-buttons").show();
+// }
 
   $(".member-container").on("mouseenter", function() {
     console.log("works");
 
-    $(".member-container").not(this).addClass("member-inactive")
+    $(".member-container").not(this).addClass("member-inactive");
     $(this).addClass("member-active");
     $(this).children(".placeholder").addClass("placeholder-active");
-    $(this).parent("#myteam").addClass("myteam-active")
+    $(this).parent("#myteam").addClass("myteam-active");
 
-    $(this).children(".gender").show();
+    $(this).find(".gender").fadeIn();
+    $(this).find(".member-detail").fadeIn();
+    $(this).find(".ability").fadeIn();
+    $(this).find(".nature").fadeIn();
+    $(this).find(".stats").fadeIn();
+    $(this).find(".moves").fadeIn();
+    $(this).find(".edit-buttons").fadeIn();
+
   })
 
   $(".member-container").on("mouseleave", function() {
@@ -17,5 +35,13 @@ $(document).ready(function() {
     $(".member-container").removeClass("member-inactive")
     $(this).children(".placeholder").removeClass("placeholder-active");
     $(this).parent("#myteam").removeClass("myteam-active")
+
+    $(this).find(".gender").fadeOut();
+    $(this).find(".member-detail").fadeOut();
+    $(this).find(".ability").fadeOut();
+    $(this).find(".nature").fadeOut();
+    $(this).find(".stats").fadeOut();
+    $(this).find(".moves").fadeOut();
+    $(this).find(".edit-buttons").fadeOut();
   })
 });
